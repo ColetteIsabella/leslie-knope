8161e7ca2f2bbce14483c05463f0053a9ffaac25
Colette Isabella <coletteisabella@gmail.com>
2015-02-24 19:54:22 -0800

    first commit

---
+++ wordlist.js
@@ -0,0 +1,23 @@
+var compliment = [
+"beautiful",
+"radiant",
+"proud"
+];
+
+var insult = [
+"naive",
+"complicated",
+"consistent"
+];
+
+var adjective = [
+"baby",
+"gorgeous",
+"perfect"
+];
+
+var animal [
+"hippo",
+"penguin",
+"orangutan",
+];
